78c7f8ea578f757a3ada1ec643c9835017a75033
vitalii ivantsiv <vitalii.ivantsiv@edvantis.com>
2020-01-21 16:59:10 +0200

    Changed default layout. Added nodemailer and nodemon.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const nodemailer = require('nodemailer');
+
+let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: '',
+        pass: ''
+    }
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'LEOSPRINT' });
+});
+
+
+router.post('/sendEmail', function (req, res, next) {
+    let mailOptions = {
+        from: '',
+        to: '',
+        subject: 'Order',
+        text: `${req.body.name} \n ${req.body.text} \n ${req.body.email}`
+    };
+
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            res.status(500).send("Cannot send email");
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.sendStatus(200);
+        }
+    });
 });
 
 module.exports = router;
