4c34a5474a5b56cfebd858ae689e6a11aa03538c
vitalii ivantsiv <vitalii.ivantsiv@edvantis.com>
2020-01-22 18:28:04 +0200

    Added styles for contact us form. Added new route for cars. Added functionality for cars gallery.

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -1,8 +1,52 @@
+let cars = [];
 $(document).ready(function () {
-    $('input[type="tel"]').inputmask({ alias: "phone", "clearIncomplete": true });
+    $('#phone').mask('+380-0000-000-00');
+
+    axios.get('/cars/getAll')
+        .then(res => res.data)
+        .then(data => {
+            cars = data.cars;
+
+            initDropDown(cars);
+        })
+        .catch(err => {
+            console.log(err);
+        })
 });
 
+
+
 $("#contact-us-form").validate({
+    rules: {
+        name: {
+            required: true,
+            minlength: 2
+        },
+        email: {
+            required: true,
+            email: true,
+        },
+        phone: {
+            required: true,
+            minlength: 16,
+            maxlength: 16
+        }
+    },
+    messages: {
+        name: {
+            required: "Поле для введення імені є обов'язковим",
+            minlength: "Мінімальна довжина в два символи"
+        },
+        email: {
+            required: "Поле для введення email є обов'язковим",
+            email: "Ваш email-адрес має бути в форматі name@domain.com"
+        },
+        phone: {
+            required: "Номер телефону є обов'язковим для заповнення",
+            minlength: "Номер телефону введено в не коректному форматі",
+            maxlength: "Номер телефону введено в не коректному форматі"
+        }
+    },
     submitHandler: function(form) {
         let data = $(form).serializeArray().reduce((ac, cur) => {
                 ac[cur.name] = cur.value;
@@ -16,6 +60,7 @@ $("#contact-us-form").validate({
     invalidHandler: function(event, validator) {
         // 'this' refers to the form
         const errors = validator.numberOfInvalids();
+        console.log(errors);
         if (errors) {
             const message = errors == 1
                 ? 'You missed 1 field. It has been highlighted'
@@ -27,3 +72,44 @@ $("#contact-us-form").validate({
         }
     }
 });
+
+$('#cars-list').change(function () {
+    setCar(cars.find(car => car.id === +this.value).imgUrl);
+});
+
+function initDropDown(dataOfCars) {
+    $('#cars-list').append(
+        [
+            "<optgroup label=\"До 22-х місць\">",
+            ...dataOfCars.map((car, i) => {
+                if(i === 0) {
+                    return `<option value='${car.id}' selected>${car.name}</option>`
+                }
+
+                return `<option value='${car.id}'>${car.name}</option>`
+            }),
+            "</optgroup>"
+        ]
+    );
+
+    setCar(dataOfCars[0].imgUrl);
+}
+
+function setCar(imageUrl) {
+    $('#gallery_main').css('background-image', 'url(' + imageUrl + ')');
+}
+
+$("form#contact-us-form :input, form#contact-us-form textarea").each(function () {
+    $(this).on('input', translateInput);
+    $(this).on('click', translateInput);
+});
+
+function translateInput() {
+    let parent = $(this).parent();
+    let span = parent.find('span.input-name');
+    if(this.value && this.value !== '') {
+        span.css( 'transform', 'translateY(-22px) scale(0.8)');
+    } else {
+        span.css( 'transform', 'scaleX(1)');
+    }
+}
