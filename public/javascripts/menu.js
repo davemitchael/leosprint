3e145127787b3390ddc85124d69017e262f13655
Taras Tykhyy <tarastykhyy@gmail.com>
2020-01-25 11:48:04 +0200

    -Добавлені якорні лінки на мень, зроблений ефект закриття меню при кліку на лінк -Рефреш лого на слайдер -Забрав баг з лого на фулл-екрані

--- public/javascripts/menu.js
+++ public/javascripts/menu.js
@@ -4,4 +4,17 @@ $('document').ready(function() {
 	 $('.overlay').toggleClass('open');
 	 });
   });
-
+//Animate scroll to ->div and close menu
+  $(document).ready(function(){
+	$("#overlay").on("click","a", function (event) {
+	  var trigger1 = $('.button_container'),
+	  wrapper = $('#overlay');
+	  trigger1.removeClass('active');
+	  trigger1.addClass('is-closed');
+	   wrapper.removeClass('open');
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+  });
