ee054f8a286c0114437e771ccf1d9dbf78175a9d
Taras Tykhyy <tarastykhyy@gmail.com>
2020-02-09 18:57:52 +0200

    home-slider fixed

--- public/javascripts/homeSlider.js
+++ public/javascripts/homeSlider.js
@@ -1,64 +1,66 @@
-  $('.home-slider').slick({
-    draggable: true,
-    autoplay: true,
-    autoplaySpeed: 10000,
-    arrows: false,
-    dots: false,
-    fade: true,
-    speed: 700,
-    infinite: true,
-    mobileFirst:true,
-    cssEase: 'ease',
-    touchThreshold: 100,
-    useCSS: true,
-    pauseOnHover:false,
-    pauseOnFocus:false,
-    responsive: [
-        {
-          breakpoint: 1360, //1360->1920px
-          settings: {
-            slidesToShow: 1
-                    }
-        }, 
-        {
-          breakpoint: 1024,//1024-1360px
-          settings: {
-            slidesToShow: 1
+$('.home-slider').slick({
+  draggable: true,
+  autoplay: true,
+  autoplaySpeed: 10000,
+  lazyload:'progressive',//lazy load image
+  arrows: false,
+  dots: false,
+  fade: true,
+  speed: 700,
+  infinite: true,
+  mobileFirst:true,
+  cssEase: 'ease',
+  touchThreshold: 100,
+  useCSS: true,
+  pauseOnHover:false,
+  pauseOnFocus:false,
+  responsive: [
+      {
+        breakpoint: 1360, //1360->1920px
+        settings: {
+          slidesToShow: 1
+                  }
+      }, 
+      {
+        breakpoint: 1024,//1024-1360px
+        settings: {
+          slidesToShow: 1
+      }
+      }, 
+      {
+        breakpoint: 768,//768->1024px
+        settings: {
+          slidesToShow: 1,
+          centerMode: true,
+          autoplay: false,
+          draggable: false,
+          mobileFirst: true
         }
-        }, 
-        {
-          breakpoint: 768,//768->1024px
-          settings: {
-            slidesToShow: 1,
-            centerMode: true,
-            autoplay: false,
-            draggable: false,
-            mobileFirst: true
-          }
-        },
-        {
-          breakpoint: 320,//320->0 px
-          settings: {
-            slidesToShow: 1,
-            centerMode: true,
-            autoplay: false,
-            draggable: false,
-            mobileFirst: true
-          }
+      },
+      {
+        breakpoint: 320,//320->0 px
+        settings: {
+          slidesToShow: 1,
+          centerMode: true,
+          autoplay: false,
+          draggable: false,
+          mobileFirst: true
         }
-      ]
-  })
+      }
+    ]
+})
+
 //   .on('setPosition', function (event, slick) {
 //     slick.$slides.css('height', slick.$slideTrack.height() + 'px');
 // });
 // height adapive home-slider
 .on('setPosition', function(event, slick) {
-  slick.$slider.find(".home-slider .tile:not(.position-set)").addClass('position-set').css('height', slick.$slideTrack.height() + 'px');
+slick.$slider.find(".home-slider .tile:not(.position-set)").addClass('position-set').css('height', slick.$slideTrack.height() + 'px');
 });
-  // Script slick slider responsive resize slick slider
+// Script slick slider responsive resize slick slider
 $(window).resize(function () {
-    $('.home-slider').not('.slick-initialized').slick('resize');
-  });
-  $(window).on('orientationchange', function () {
-    $('.home-slider').not('.slick-initialized').slick('resize');
-  });
+  $('.home-slider').not('.slick-initialized').slick('resize');
+});
+$(window).on('orientationchange', function () {
+  $('.home-slider').not('.slick-initialized').slick('resize');
+});
